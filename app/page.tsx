3338acbe9201c912e88b186f822cdc23ffec613b
SIG-MEDIALUNAS <alva.proko97@gmail.com>
2026-06-07 21:12:28 -0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import ControlVolante from "@/components/checklist-medialunas";
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-slate-50">
+      <ControlVolante />
+    </main>
+  );
+}
